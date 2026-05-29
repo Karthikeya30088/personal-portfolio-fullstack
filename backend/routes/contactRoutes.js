f1b04b34f02bef5673a71acf691ec5651fee6a4a
2400030088_karthikeya <karthikeyamedisetti3006@gmail.com>
2026-05-29 11:42:36 +0530

    Update contactRoutes.js

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -4,16 +4,12 @@ const router = express.Router();
 
 const db = require("../db");
 
-/* SEND CONTACT MESSAGE */
-
 router.post("/", (req, res) => {
 
     const { name, email, message } = req.body;
 
-    console.log(req.body);
-
     const sql =
-        "INSERT INTO contacts(name, email, message) VALUES (?, ?, ?)";
+      "INSERT INTO contacts(name, email, message) VALUES (?, ?, ?)";
 
     db.query(sql, [name, email, message], (err, result) => {
 
